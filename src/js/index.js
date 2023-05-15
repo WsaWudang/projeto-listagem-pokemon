c03b167c2da6c66f34b98a066739300e29d251d6
Wesley Almeida <89345925+WsaWudang@users.noreply.github.com>
2023-05-14 23:39:32 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+alert("Aqui tem os primeiros 18 pokemons");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
